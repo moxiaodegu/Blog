31ac48d5239640757f5312c0f96ab48c690922ef
yanli_ma <yanli_ma@cyai.com>
2020-09-27 16:37:02 +0800

    note

--- JavaScript/arrary.md
+++ JavaScript/arrary.md
@@ -1,24 +1,42 @@
-# array 方法
+# Array 方法
+1. Array.from() 把类数组对象/可迭代对象浅拷贝创建一个新的数组
+2. Array.isArray()判断是否是数组
+3. Array.of()
+# Array 实例方法
+
+### 修改器方法
+
+1. push 末尾添加数组，返回数组长度 （一个或多个）
+2. pop 末尾删除数组，返回数组长度
+3. shift 移除数组第一个元素，并返回该元素
+4. unshift 在数组开头添加一个或者多个元素，返回数组新长度
+5. splice 删除元素
+6. reverse 颠倒数组的顺序slic
+7. sort 数组排序，也可有回调函数，判断如何排序数组
+
+### 访问方法
 1. concat 连接两个数组，并返回一个新数组。
-2. join 将数组元素连接成一个字符串。
-3. push 末尾添加数组，返回数组长度 （一个或多个）
-4. pop 末尾删除数组，返回数组长度
-5. shift 移除数组第一个元素，并返回该元素
-6. unshift 在数组开头添加一个或者多个元素，返回数组新长度
-7. slice 从数组移除或替换一些元素
-8. reverse 颠倒数组的顺序
-9. sort 数组排序，也可有回调函数，判断如何排序数组
-10. indexOf/lastIndexOf 在数组中搜索，并返回索引(indexOf:第一次出现的位置、lastIndexOf最后一次出现的位置)
-11. forEach 在每个数组元素上执行callback
-12. map 创建一个新数组，数组元素是元素组通过执行callback得来
-13. filter 返回一个在回调函数上返回true的元素的新数组
-14. every 当数组的每一个元素在回调函数上都返回true就返回true，否则返回false
-15. some 只要一个数组中一个元素返回true就返回true，否则返回false
+2. indexOf/lastIndexOf 在数组中搜索，并返回索引(indexOf:第一次出现的位置、lastIndexOf最后一次出现的位置)
+3. includes 判断数组是否有当前值
+4. join 将数组元素连接成一个字符串。
+5. slice 抽取当前数组中的一段元素组合成一个新数组。
+
+### 迭代方法
+1. forEach 在每个数组元素上执行callback
+2. map 创建一个新数组，数组元素是元素组通过执行callback得来
+3. filter 返回一个在回调函数上返回true的元素的新数组
+4. every 当数组的每一个元素在回调函数上都返回true就返回true，否则返回false
+5. some 只要一个数组中一个元素返回true就返回true，否则返回false
 注：以上回调函数传入三个值，第一个为当前元素，第二个为当前元素索引，第三个为数组本身。
-16. reduce 方法接收一个函数作为累加器，数组中的每个值（从左到右）开始缩减，最终计算为一个值。
+6. reduce 方法接收一个函数作为累加器，数组中的每个值（从左到右）开始缩减，最终计算为一个值。
     - [10,20,30].reduce( (first, second) => return first + second ) 返回 60；
 	- arr.reduce(callback(accumulator（累计器）, currentValue(当前值),[, index（当前索引）[, array（原始数组）]])[, initialValue（初始值）])
 	- redeceRight: 和reduce类似，从右至左开始
+7. find 返回第一个符合的元素，找不到undefined
+8. findIndex 返回第一个符合元素的下标，找不到undefined
+9. keys 返回所有索引键的对象
+10. values 返回包含所有值的对象
+11. entries 返回键值对的对象
 
 # foreach 和 map 的区别
  - map分配内存空间存储新数组并返回，forEach()不会返回数据。
@@ -37,6 +55,9 @@
 				return now+all
 			},''); // 字符串倒叙
 
+# 数组泛型方法
+
+
 # 数组推导式
 
 # 数组选取最大值
@@ -44,6 +65,8 @@
 2. Math.max.apply(...arr)
 3. sort()
 4. reduce
+
+
 
 
 
--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -2,117 +2,131 @@
  * 1. event loop 考察
  * 结果：start end 1 2 3 7 8 
 */
-console.log("start")
-async function asyncFun() {
-	await new Promise((resolve, reject) => {
-		setTimeout( () => {
-			console.log(22222)
-			resolve()
-		}, 0 )
-	}).then( () => {
-		console.log(33333)
-	})
+// console.log("start")
+// async function asyncFun() {
+// 	await new Promise((resolve, reject) => {
+// 		setTimeout( () => {
+// 			console.log(22222)
+// 			resolve()
+// 		}, 0 )
+// 	}).then( () => {
+// 		console.log(33333)
+// 	})
 
-	console.log(eeeee)
-}
-asyncFun()
+// 	console.log(eeeee)
+// }
+// asyncFun()
 
-new Promise( (resolve,reject) => {
-	console.log(44444444)
-	resolve()
-}).then( () => {
-	console.log(55555555555)
-})
-setTimeout(()=>{
-    console.log('1')
-    new Promise(function(resolve){
-     console.log(" 2 ")
-      resolve();
-}).then(function() {
-        console.log('3')
-    })
-}, 0)
-setTimeout(()=>{
-    console.log('7')
-    Promise.resolve().then(function() {
-        console.log('8')
-    })
-}, 0)
-console.log("end")
+// new Promise( (resolve,reject) => {
+// 	console.log(44444444)
+// 	resolve()
+// }).then( () => {
+// 	console.log(55555555555)
+// })
+// setTimeout(()=>{
+//     console.log('1')
+//     new Promise(function(resolve){
+//      console.log(" 2 ")
+//       resolve();
+// }).then(function() {
+//         console.log('3')
+//     })
+// }, 0)
+// setTimeout(()=>{
+//     console.log('7')
+//     Promise.resolve().then(function() {
+//         console.log('8')
+//     })
+// }, 0)
+// console.log("end")
 
-/**
- * 字符串颠倒输出
- */
- Array.prototype.reduce.call('abcdefg', (prve, cur) => cur + prve)
+// /**
+//  * 字符串颠倒输出
+//  */
+//  Array.prototype.reduce.call('abcdefg', (prve, cur) => cur + prve)
 
- /** 
-  * 数组交集 并集
- */
+//  /** 
+//   * 数组交集 并集
+//  */
 
- let temp1 = [2,3,4,5,6,7,8];
- let temp2 = [1,3,5,6,7,9,8,2]
+//  let temp1 = [2,3,4,5,6,7,8];
+//  let temp2 = [1,3,5,6,7,9,8,2]
  
- // 交集
- temp1.filter( e => new Set(temp2).has(e))
- // 差集
- temp1.filter( e => !(new Set(temp2).has(e)))
- //  并集
- new Set([...temp1, ...temp2])
+//  // 交集
+//  temp1.filter( e => new Set(temp2).has(e))
+//  // 差集
+//  temp1.filter( e => !(new Set(temp2).has(e)))
+//  //  并集
+//  new Set([...temp1, ...temp2])
 
- /** 
-  * 数组去重
- */
-let repeat1 = [1,2,1,2,4,5,6,4,6,7,8,7,6]
-let repeat2 = [...new Set(repeat1)]
+//  /** 
+//   * 数组去重
+//  */
+// let repeat1 = [1,2,1,2,4,5,6,4,6,7,8,7,6]
+// let repeat2 = [...new Set(repeat1)]
 
-/** 
- * 原型链
-*/
-var arr = Array()
-arr.__proto__
-// [constructor: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, find: ƒ, …]concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {copyWithin: true, entries: true, fill: true, find: true, findIndex: true, …}__proto__: Object
-arr.__proto__ === Array.prototype === arr.constructor.prototype
-// true
-Array.prototype.__proto__ === Object.prototype
-// true
-Object.prototype.__proto__
-// null
+// /** 
+//  * 原型链
+// */
+// var arr = Array()
+// arr.__proto__
+// // [constructor: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, find: ƒ, …]concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {copyWithin: true, entries: true, fill: true, find: true, findIndex: true, …}__proto__: Object
+// arr.__proto__ === Array.prototype === arr.constructor.prototype
+// // true
+// Array.prototype.__proto__ === Object.prototype
+// // true
+// Object.prototype.__proto__
+// // null
 
-var restoreString = function(s, indices) {
-    var map = new Map();
-    let str = '';
-    Array.prototype.forEach.call(s,(e,i)=> {
-        map.set(indices[i],e)
-    })
-	let len = map.keys()
-	console.log(len)
-	len.sort( (a, b) => a-b).forEach( e => {
-        str += map.get(e)
-    })
+// var restoreString = function(s, indices) {
+//     var map = new Map();
+//     let str = '';
+//     Array.prototype.forEach.call(s,(e,i)=> {
+//         map.set(indices[i],e)
+//     })
+// 	let len = map.keys()
+// 	console.log(len)
+// 	len.sort( (a, b) => a-b).forEach( e => {
+//         str += map.get(e)
+//     })
     
-    return str
+//     return str
     
-};
+// };
 
-var restoreString = function(s, indices) {
-	let arr = new Array(s.length);
-	console.log(arr)
-    for (let i = 0; i < indices.length; i++) {
-		console.log(arr[indices[i]],s[i])
-		arr[indices[i]] = s[i]
-	};
-    return arr.join("");
-};
+// var restoreString = function(s, indices) {
+// 	let arr = new Array(s.length);
+// 	console.log(arr)
+//     for (let i = 0; i < indices.length; i++) {
+// 		console.log(arr[indices[i]],s[i])
+// 		arr[indices[i]] = s[i]
+// 	};
+//     return arr.join("");
+// };
 
-var smallerNumbersThanCurrent = function(nums) {
-    const sorter = (a,b) => a-b;
-    const mapper = (v,i) => [v,i];
-	const tempMap = new Map([...nums].sort(sorter).map(mapper).reverse());
-	console.log(nums)
-	console.log([...nums])
-	console.log([...nums].sort(sorter))
-	console.log([...nums].sort(sorter).map(mapper))
-	console.log([...nums].sort(sorter).map(mapper).reverse())
-	console.log(tempMap)
-    return nums.map(x => tempMap.get(x));
-};
+// var smallerNumbersThanCurrent = function(nums) {
+//     const sorter = (a,b) => a-b;
+//     const mapper = (v,i) => [v,i];
+// 	const tempMap = new Map([...nums].sort(sorter).map(mapper).reverse());
+// 	console.log(nums)
+// 	console.log([...nums])
+// 	console.log([...nums].sort(sorter))
+// 	console.log([...nums].sort(sorter).map(mapper))
+// 	console.log([...nums].sort(sorter).map(mapper).reverse())
+// 	console.log(tempMap)
+//     return nums.map(x => tempMap.get(x));
+// };
+
+var test = () => {
+	console.log(new Object('123'))
+	console.log(new Object(123))
+	console.log(new Object(function(){}))
+}
+
+test();
+
+let circle = function () {};
+let shape = {};
+let circle = new circle();
+
+
